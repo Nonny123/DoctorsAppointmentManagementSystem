b9a03ad8d95fdb2327d7bfb7543f34cc792086ce
Nonso <justcoolk@yahoo.com>
2022-01-02 06:53:43 +0100

    Separated admin registration and login from other users

--- DAMS/Controllers/AccountController.cs
+++ DAMS/Controllers/AccountController.cs
@@ -88,8 +88,14 @@ namespace DAMS.Controllers
                 if (result.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(user, model.RoleName);
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("Index", "Home");
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    // return RedirectToAction("Index", "Home");
+                    if (!User.IsInRole(Helper.Admin))
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                    }
+                    
+                    return RedirectToAction("Index", "Appointment");
                 }
                 foreach (var error in result.Errors)
                 {
--- DAMS/Utility/Helper.cs
+++ DAMS/Utility/Helper.cs
@@ -31,14 +31,24 @@ namespace DAMS.Utility
         public static int success_code = 1;
         public static int failure_code = 0;
 
-        public static List<SelectListItem> GetRolesForDropDropdown()
+        public static List<SelectListItem> GetRolesForDropDropdown(bool isAdmin)
         {
-            return new List<SelectListItem>
+            if (isAdmin)
             {
-                new SelectListItem{Value=Helper.Admin, Text=Helper.Admin},
-                new SelectListItem{Value=Helper.Patient, Text=Helper.Patient},
-                new SelectListItem{Value=Helper.Doctor, Text=Helper.Doctor}
-            };
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Admin, Text=Helper.Admin}
+                };
+
+            }
+            else
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Patient, Text=Helper.Patient},
+                    new SelectListItem{Value=Helper.Doctor, Text=Helper.Doctor}
+                };
+            }
         }
 
         public static List<SelectListItem> GetTimeDropDown()
