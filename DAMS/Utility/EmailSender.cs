9332f5fe7747f3a0aafe4daf72eba65a5be8cd65
Nonso <justcoolk@yahoo.com>
2022-01-02 08:45:55 +0100

    Integrated Mailjet API for sending Emails

---
+++ DAMS/Utility/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAMS.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("e0a5c4941c1242c6a4291e1c1ff39f23", "330e6d7c63c7c20cd8dd4eeb5febaec7")
+            {
+                
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+               .Property(Send.Messages, new JArray {
+                new JObject {
+                 {"From", new JObject {
+                  {"Email", "devnonny@gmail.com"},
+                  {"Name", "Appointment Scheduler"}
+                  }},
+                 {"To", new JArray {
+                  new JObject {
+                   {"Email", email},
+                   
+                   }
+                  }},
+                 {"Subject", subject},
+                 
+                 {"HTMLPart", htmlMessage}
+                 }
+                   });
+            MailjetResponse response = await client.PostAsync(request);
+        }
+    }
+}
